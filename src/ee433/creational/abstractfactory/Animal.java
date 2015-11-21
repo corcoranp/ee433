2c52d5731d78c96322865a010859025ceb3ab6ed
Peter Corcoran <pmcorcor@gmail.com>
2015-11-21 17:22:33 -0600

    updated animal with structure id

--- src/ee433/creational/abstractfactory/Animal.java
+++ src/ee433/creational/abstractfactory/Animal.java
@@ -1,9 +1,9 @@
 /**
- *  File: animal.java<br/>
+ *  File: Animal.java<br/>
  *  Author: Peter M. Corcoran, pmcorcor@uab.edu<br/>
  *  Assignment: EE433 Research Project<br/>
  *  Version: 1.0.0 11/21/2015 pmc - initial coding<br/>
- *	
+ *	Structure Component: Abstract Product
  */
 
 package ee433.creational.abstractfactory;
